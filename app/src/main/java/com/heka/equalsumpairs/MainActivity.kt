2c27522f63d2f8dd2bd3b1946596d01ecff63448
htk007 <hasankaya921@hotmail.com>
2023-04-14 23:15:28 +0300

    initial commit

---
+++ app/src/main/java/com/heka/equalsumpairs/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.heka.equalsumpairs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val equalSumPairs = findEqualSumPairs(arr)
+        Log.i("HASANLOG",equalSumPairs.toString());
+    }
+
+    fun findEqualSumPairs(arr: IntArray): List<Pair<Int, Int>> {
+        val result = mutableListOf<Pair<Int, Int>>()
+        val sumIndicesMap = HashMap<Int, MutableList<Int>>()
+
+        for (i in arr.indices) {
+            for (j in i + 1 until arr.size) {
+                val sum = arr[i] + arr[j]
+                if (sum in sumIndicesMap) {
+                    for (index in sumIndicesMap[sum]!!) {
+                        result.add(Pair(arr[index], arr[i]))
+                        result.add(Pair(arr[index], arr[j]))
+                    }
+                }
+                if (sum !in sumIndicesMap) {
+                    sumIndicesMap[sum] = mutableListOf(i)
+                } else {
+                    sumIndicesMap[sum]!!.add(i)
+                }
+            }
+        }
+
+        return result
+    }
+
+}
